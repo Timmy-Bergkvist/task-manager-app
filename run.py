0357df2ce727ae863c33d491a30a73590b57254d
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-05-27 19:36:13 +0200

    added html

--- run.py
+++ run.py
@@ -1,11 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manger'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+#MONGO_URI = os.environ.get("MONGO_URI")
+#app.config["MONGO_URI"] = 'mongodb+srv://root:<password>@mycluster-pcvdt.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "hello world"
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
